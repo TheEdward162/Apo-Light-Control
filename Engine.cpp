0d355c9df875d1bed4b87aac89e1202a879389f9
Eduard Lavuš <lavusedu@fel.cvut.cz>
2018-04-14 17:36:58 +0200

    Structurized code. This will probably cause merge conflicts.

--- Engine.cpp
+++ Engine.cpp
@@ -28,29 +28,48 @@ void checkArguments(int argc, char** argv) {
 	}
 }
 
+// variables
+LightUnit thisUnit;
+std::thread networkThread;
+
 void NetworkThreadRun(std::future<void> death) {
 	// init sockets
 	NetworkHandler networkHandler = NetworkHandler();
 
-	// send broadcast
-	// recieve broadcast
-	// send control messages
 	int i = 0;
 	while (death.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
-		std::cout << "i: " << i << std::endl;
-		i++;
+		// send broadcast
+		// send control messages
+		// recieve control messages and execute them
+
+		// recieve broadcasts for some time
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
+}
+
+void mainLoop() {
+	// main loop
+
+	while (true) {
+		// poll input events
+		// execute stuff based on user input
+		// draw pretty pictures
+		break;
+	}
+
+	// Boss is coming around. Quick, pretend to be working...
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 int Engine::run(int argc, char** argv) {
 	checkArguments(argc, argv);
 	
 	// init this unit object
-	LightUnit thisUnit = LightUnit(argv[1]);
+	thisUnit = LightUnit(argv[1]);
 	IOTools::loadImage16x16(argv[2], thisUnit.image);
 
+	// for test.ppm this should be 0000 f800 07e0 001f ffff
 	printf("%x %x %x %x %x\n", thisUnit.image[0], thisUnit.image[1], thisUnit.image[2], thisUnit.image[3], thisUnit.image[4]);
 
 	// UI thread will handle drawing and input events
@@ -62,17 +81,13 @@ int Engine::run(int argc, char** argv) {
 
 	// stateless death event, yaay
 	std::promise<void> deathPromise;
-	std::thread networkThread(NetworkThreadRun, deathPromise.get_future());
+	networkThread = std::thread(NetworkThreadRun, deathPromise.get_future());
 
-	// main loop
-	// poll input events
-	// execute stuff based on user input
-	// draw pretty pictures
+	mainLoop();
 
-	std::this_thread::sleep_for(std::chrono::seconds(1));
 	// Death is a comin'
 	deathPromise.set_value();
-
 	networkThread.join();
+
 	return 0;
 }
