b5f66773474f35aceb5d1d3a9605ccd87cf9536f
Eduard Lavuš <lavusedu@fel.cvut.cz>
2018-04-14 17:12:38 +0200

    Threading basics.

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,11 @@
 #include <stdexcept>
 #include <cstring>
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <future>
+
 #include "Engine.h"
 
 #include "Misc/IOTools.h"
@@ -23,24 +28,51 @@ void checkArguments(int argc, char** argv) {
 	}
 }
 
-void Engine::run(int argc, char** argv) {
-	checkArguments(argc, argv);
+void NetworkThreadRun(std::future<void> death) {
+	// init sockets
+	NetworkHandler networkHandler = NetworkHandler();
 
-	// create UI thread
-	// init UI
+	// send broadcast
+	// recieve broadcast
+	// send control messages
+	int i = 0;
+	while (death.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+		std::cout << "i: " << i << std::endl;
+		i++;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
+int Engine::run(int argc, char** argv) {
+	checkArguments(argc, argv);
 	
 	// init this unit object
 	LightUnit thisUnit = LightUnit(argv[1]);
 	IOTools::loadImage16x16(argv[2], thisUnit.image);
 
-	// init sockets
-	NetworkHandler networkHandler = NetworkHandler();
+	printf("%x %x %x %x %x\n", thisUnit.image[0], thisUnit.image[1], thisUnit.image[2], thisUnit.image[3], thisUnit.image[4]);
+
+	// UI thread will handle drawing and input events
+	// Network thread will handle network communication, e.g. broadcasting and control messaging
+	// Synchronization will work with mutexes on shared objects
+	// 
+	// Communication between threads (e.g. send control message, please, etc.) needs to be figured out.
+	// Promises? Sounds... *puts on glasses* ...promising. Check http://en.cppreference.com/w/cpp/thread/promise
+
+	// stateless death event, yaay
+	std::promise<void> deathPromise;
+	std::thread networkThread(NetworkThreadRun, deathPromise.get_future());
 
 	// main loop
-	// send broadcast
-	// recieve broadcast
 	// poll input events
-	// send control messages
+	// execute stuff based on user input
+	// draw pretty pictures
 
-	printf("%x %x %x %x %x\n", thisUnit.image[0], thisUnit.image[1], thisUnit.image[2], thisUnit.image[3], thisUnit.image[4]);
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	// Death is a comin'
+	deathPromise.set_value();
+
+	networkThread.join();
+	return 0;
 }
